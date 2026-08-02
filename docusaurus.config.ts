ab86ba2a95c58ca249712c6f3e76978289b3fa36
Rahul Raj <rahulraj18081994@gmail.com>
2026-08-02 20:01:47 +0100

    fix: Update base URL in docusaurus.config.ts for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://mycerebral.space',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
