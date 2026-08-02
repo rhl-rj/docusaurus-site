6126c9fd7e9f05ec92a0cdbc687179637e6c5fd7
Rahul Raj <rahulraj18081994@gmail.com>
2026-08-02 18:22:42 +0100

    fix: Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://rhl-rj.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rhl-rj', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-site',
+  deploymentBranch: 'deployment', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
